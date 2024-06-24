7ad1a7026a267ad759c0881911a1ef33bd3ab363
Weyman Wong <wxwong806@gmail.com>
2024-06-25 10:34:11 +1200

    Added javadoc comments to the interface

--- app/src/main/java/com/example/fixitfitness/fitnessroutine/FitnessSession.java
+++ app/src/main/java/com/example/fixitfitness/fitnessroutine/FitnessSession.java
@@ -15,7 +15,19 @@ public interface FitnessSession {
      */
     String getDescription();
 
+    /**
+     * Returns a copy of the fitness session instance
+     *
+     * @return a copy of the fitness session instance
+     */
     FitnessSession copy();
 
+    /**
+     * Returns a string representation of the session.
+     * Should have a corresponding FitnessSession(String sessionString) constructor to convert the
+     * string to a session class .
+     *
+     * @return a string representing the current session instance
+     */
     String bundleToString();
 }
